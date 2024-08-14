745c4aa908f60245319b5996beae420bc12c078e
Mateus Caden Coelho <mateus.caden@quipux.com.br>
2024-08-14 11:35:08 -0300

    Finalizando clean install v0.1

--- updates/clean_install.py
+++ updates/clean_install.py
@@ -1,36 +1,74 @@
-import datetime
 import json
-import sys
-import subprocess
+import autoit
+import time
 
-def filtrar_projetos_back(projeto):
-    if "caminhoGulp" not in projeto:
-        return True
-    else:
-        return False
 
-with open("../paths/caminhos.json", "r") as jsonFile:
+def map_debug(__config):
+    return __config['debug_config']
+
+
+def map_mvn(config):
+    return config['mvn_update'] if 'mvn_update' in config else ''
+
+
+def run_mvn(mvns, possiveis_mvn):
+    old_index = 0
+
+    autoit.send("!{F5}")
+    autoit.win_wait_active("Update Maven Project")
+    autoit.control_click("Update Maven Project", "Button3")
+    autoit.control_click("Update Maven Project", "Button8")
+    autoit.control_focus("Update Maven Project", "SysTreeView321")
+    for mvn in mvns:
+        index_atual = possiveis_mvn.index(mvn)
+        tabs = index_atual - old_index
+        autoit.send("{DOWN " + str(tabs) + "}")
+        autoit.send('{SPACE}')
+        old_index = index_atual
+    autoit.control_click("Update Maven Project", "Button12")
+    time.sleep(1)
+
+
+def run_config(config_atual, fim):
+    janela_debug = "Debug Configurations"
+
+    autoit.send("!r")  # Digita ALT R
+    autoit.send("b")  # Digita B
+    autoit.win_wait_active(janela_debug)  # Espera a janela de debug config abrir
+    autoit.control_focus(janela_debug, "Edit1")  # Foca no input de busca
+    autoit.control_send(janela_debug, "Edit1", config_atual)  # Digita a config que rodará
+    time.sleep(0.2)  # Espera a busca acontecer
+    autoit.send("{ENTER}")  # Seleciona a config encontrada
+    time.sleep(0.2)  # Espera para garantir que a config foi selecionada
+    autoit.send("{ENTER}")  # Roda a config
+    time.sleep(1)  # Espera 1s para não pegar o texto da run anterior
+
+    texto_console = autoit.control_get_text("GA - Spring", "Static1")  # Pega o valor do texto acima do console
+    print(texto_console)
+    while "terminated" not in texto_console and fim is False:  # Se o texto possuir "terminated", quebra o loop, caso contrário, espera e pega novamente
+        time.sleep(0.5)
+        texto_console = autoit.control_get_text("GA - Spring", "Static1")
+        print(texto_console)
+
+
+with open("../paths/debug-config.json", "r") as jsonFile:
     data = json.load(jsonFile)
 
-projetos = filter(filtrar_projetos_back, data['projetos'])
+configs = data['configs']
+possiveis_mvn = data['possiveis_mvn']
 
-with open("../logs/log_clean_install.log", "w") as log_file:
-    log_file.write(datetime.date.today().strftime('%d/%m/%Y') + " - Start\n")
+autoit.opt("WinTitleMatchMode", 2) # Opção para só precisar buscar parte do nome da janela, ao invés do nome inteiro, que pode mudar
+autoit.win_activate("GA - Spring")  # Abre a janela do STS
+autoit.win_wait_active("GA - Spring")  # Espera abrir antes de continuar
 
-    for projeto in projetos:
-        print("Atualizando: " + projeto['nome'])
-        log_file.write("---------- Projeto: " + projeto['nome'] + "----------\n")
-        for index, servico in enumerate(projeto['servicos']):
-            try:
-                saida_log = subprocess.check_output(["mvn", "clean", "install", "-DskipTests"], shell=True, stderr=subprocess.STDOUT, universal_newlines=True, cwd=servico['caminho'])
-                log_file.write(servico['nome'] + " - OK")
-                print(servico['nome'] + " - OK (" + str(index + 1) + "/" + str(len(projeto['servicos'])) + ")")
-                log_file.write("\n" + saida_log + "\n")
-            except subprocess.CalledProcessError as e:
-                print("\033[91m" + servico['nome'] + " - ERROR! (" + str(index + 1) + "/" + str(len(projeto['servicos'])) + ")" + "\033[0m")
-                log_file.write(servico['nome'] + " - ERROR")
-                log_file.write("\n" + e.output)
+# Rodando os maven update projects
+mvns = list(map(map_mvn, configs))  # Pega uma lista com todos os mvns que rodarão
+mvns = list(filter(None, mvns))  # Limpa valores vazios
+mvns.sort()
+run_mvn(mvns, possiveis_mvn)
 
-input("JARs atualizados! Aperte ENTER para fechar esta janela.")
-
-sys.exit()
+# Rodando as debug configs
+debug_configs = list(map(map_debug, configs))
+for index, config in enumerate(debug_configs):  # Roda as configs
+    isFim = index == len(configs) - 1
+    run_config(config, isFim)
